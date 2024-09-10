fe751e6c0b5087d36a38930cb0624ce2823d9504
Akash-nath29 <anath5440@gmail.com>
2024-09-11 00:31:35 +0530

    Bug Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar'
-import Hero from './components/Hero';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import CreatePost from "./components/CreatePost";
 import Help from "./components/Help";
 
 const App = () => {
   return (
-    <Router>
+    <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/create" element={<CreatePost />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
-      {/* <Footer /> */}
-    </Router>
-  )
-}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/help" element={<Help />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
